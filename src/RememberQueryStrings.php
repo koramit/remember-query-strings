49f7b3cdcafb90010d7f8ebd17d84a589f44ef0b
Koramit Pichanaharee <koramit@users.noreply.github.com>
2021-09-15 19:24:13 +0700

    Filter out query name starts with slash.
    When host application as a subdirectory ('example.com/myblog'), the `$request` also captures last path of the url like `/posts` as a data.
    
    Remove them to make it works as expect for this use case.

--- src/RememberQueryStrings.php
+++ src/RememberQueryStrings.php
@@ -12,7 +12,11 @@ class RememberQueryStrings
             return $next($request);
         }
 
-        if (empty($request->all())) {
+        $queryFiltered = array_filter($request->all(), function ($key) {
+            return ! str_starts_with($key, '/');
+        }, ARRAY_FILTER_USE_KEY);
+        
+        if (empty($queryFiltered)) {
             return $this->remembered($next, $request);
         }
 
